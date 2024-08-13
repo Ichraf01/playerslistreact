75e465b132b1f1bdc72607e9b23d0dc3ec50d816
Ichraf <ichrafdarghouth1818@gmail.com>
2024-08-13 16:28:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PlayerList from './PlayerList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1 className="text-center mt-4" style={{color:"red", textDecoration: "underline"}}>Player List</h1>
+      <PlayerList/>
+    </div>
+  );
+}
+
+export default App;
